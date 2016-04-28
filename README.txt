4652d7e6dcc0bb8ad6954b467ae057c177bc8212
Adam-Michal-Prague <IV_Elements_TV@vesperton.com>
2016-04-28 05:07:31 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/dancingdragonprague/pen/JXmXoz.
+
+ A draft version of my portfolio
+
+Forked from [Laurent Harel](http://codepen.io/lorent/)'s Pen [lorent.io](http://codepen.io/lorent/pen/wGzzLw/).
---
+++ js/index.js
@@ -0,0 +1,43 @@
+/* ---------------------------------------------
+ Height 100%
+ --------------------------------------------- */
+function js_height_init(){
+    (function($){
+        $(".js-height-full").height($(window).height());
+        $(".js-height-parent").each(function(){
+            $(this).height($(this).parent().first().height());
+        });
+    })(jQuery);
+}
+
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+      
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
